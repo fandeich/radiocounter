fac89d706615bbda178953c554c8b253a0428411
fandeich <fandeichdani@gmail.com>
2022-02-22 15:54:33 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module radio_counter
+
+go 1.17
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/robfig/cron/v3"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	RadioHeart = "https://a4.radioheart.ru/api/json?userlogin=user8042&api=current_listeners"
+	MyRadio24  = "http://myradio24.com/users/meganight/status.json"
+)
+
+type dbType struct {
+	id    int
+	time  time.Time
+	count int
+	err   bool
+}
+
+func GetListenersRadioHeart(name string) (num int, err error) {
+	url := RadioHeart
+	var netClient = http.Client{}
+	res, err := netClient.Get(url)
+	if err != nil {
+		return -1, err
+	}
+	var result map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&result)
+	num, _ = strconv.Atoi(result[name].(string))
+	return
+}
+
+func GetListenersMyRadio24(name string) (num int, err error) {
+	url := MyRadio24
+	var netClient = http.Client{}
+	res, err := netClient.Get(url)
+	if err != nil {
+		return -1, err
+	}
+	var result map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&result)
+	num = int(result[name].(float64))
+	return
+}
+
+func GetListeners() (num int, err error) {
+	num1, err1 := GetListenersRadioHeart("listeners")
+	num2, err2 := GetListenersMyRadio24("listeners")
+	if err1 != nil {
+		err = err1
+	}
+	if err2 != nil {
+		err = err2
+	}
+	num = num1 + num2
+	return
+}
+
+func RunEveryMin() (count int) {
+	count, _ = GetListeners()
+	return
+}
+
+func RunEveryHour() (count int) {
+	ch := make(chan int)
+	count = RunEveryMin()
+
+	cr := cron.New(cron.WithLocation(time.Now().Location()))
+	cr.AddFunc("@every 1m", func() { count += RunEveryMin() })
+	cr.AddFunc("@every 59m10s", func() { ch <- 1 })
+	cr.Start()
+
+	<-ch
+
+	count = count / 4
+	cr.Stop()
+	return
+}
+
+func InsertDB(db *sql.DB, time time.Time, count int, err bool) (flag error) {
+	query := `
+			INSERT INTO main (date,counter,err)
+			VALUES ( $1, $2, $3);
+		`
+	result, flag := db.Exec(query, time, count, err)
+	_ = result
+	return
+}
+
+func CreateDB() *sql.DB {
+	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=db_radio_counter sslmode=disable"
+	db, err := sql.Open("pgx", dsn) // *sql.DB
+	if err != nil {
+		log.Fatalf("failed to load driver: %v", err)
+	}
+	if err := db.PingContext(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+	db.SetMaxOpenConns(4)
+	db.SetMaxIdleConns(4)
+
+	return db
+}
+
+func ZeroRows(db *sql.DB, TimeNow time.Time) {
+	query := `
+		select * from main
+		where id = (select max(id) from main)
+	`
+	rows, _ := db.Query(query)
+	defer rows.Close()
+
+	difference := 0
+	TimeI := time.Time{}
+	middle := 0
+	if rows.Next() {
+		date := dbType{}
+		rows.Scan(&date.id, &date.time, &date.count, &date.err)
+
+		date.time = time.Date(date.time.Year(), date.time.Month(), date.time.Day(), date.time.Hour(), 0, 0, 0, time.UTC)
+		TimeNow = time.Date(TimeNow.Year(), TimeNow.Month(), TimeNow.Day(), TimeNow.Hour(), 0, 0, 0, time.UTC)
+
+		TimeI = time.Date(date.time.Year(), date.time.Month(), date.time.Day(), date.time.Hour(), 0, 0, 0, time.UTC)
+		TimeI = TimeI.Add(time.Hour * 1)
+
+		difference = int(TimeNow.Sub(date.time).Hours())
+		middle = date.count
+	}
+
+	rand.Seed(12)
+	for i := 0; i < difference; i++ {
+		InsertDB(db, TimeI, middle+rand.Intn(50), true)
+		TimeI = TimeI.Add(time.Hour * 1)
+	}
+}
+
+func main() {
+
+	db := CreateDB()
+	defer db.Close()
+
+	TimeNow := time.Now()
+	ch := make(chan int)
+
+	cr := cron.New(cron.WithLocation(time.Now().Location()))
+	cr.AddFunc("@hourly", func() {
+		InsertDB(db, time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), 0, 0, 0, time.UTC), RunEveryHour(), false)
+	})
+	cr.Start()
+	ZeroRows(db, TimeNow)
+	<-ch
+	cr.Stop()
+	fmt.Println("Ending")
+	RunEveryHour()
+
+}
